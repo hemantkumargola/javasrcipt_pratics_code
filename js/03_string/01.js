047c5c36a87a2aed0a150e0cf3ca2f2db244af4d
hemant kumar gola <hemantgola421@gmail.com>
2025-10-21 21:57:23 +0530

    first commit

---
+++ js/03_string/01.js
@@ -0,0 +1,44 @@
+// let str = "hemant"
+// let str2 = 'golo'
+// console.log(str, typeof str);
+// console.log(str2, typeof str2);
+
+
+// let str = new String("hemant kumar gola");
+// console.log(str);
+
+// let str = "hellow how are you \n i ma fine "
+// console.log(str, str.length);
+
+
+// let str = "hemsant kumasr gols "
+// for (let key of str) {
+//     console.log(key);
+
+// }
+// for (let key in str) {
+//     console.log(key);
+
+// }
+
+// let str2 = ""
+// let str = "hemsant kumasr gols "
+// for (let key of str) {
+//     str2 = str2 + key + " "
+
+
+// }
+// console.log(str2);]
+// let name = " hemant"
+// let midname = "kumar"
+// let endname = "gola"
+// let fullname2 = name + midname + endname
+// console.log(fullname2);
+
+// let fullname = name.concat(midname, endname)
+// console.log(fullname);
+
+// console.log(midname.indexOf("m"));
+
+let str = "hemant kumar gola "
+console.log(str.split(" "));
